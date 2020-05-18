6ba6e2d2e54990d28bdb4aa338cd3c667d9255f5
liuzhuoren <liuzhuoren@utry.cn>
2020-05-18 10:57:54 +0800

    use Json

--- VertxAutoSwagger/src/main/java/io/vertx/VertxAutoSwagger/Model/Product.java
+++ VertxAutoSwagger/src/main/java/io/vertx/VertxAutoSwagger/Model/Product.java
@@ -3,28 +3,16 @@ package io.vertx.VertxAutoSwagger.Model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import generator.Required;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Product {
 
     @Required
-    @JsonProperty(value = "_id", required = true)
     public String _id;
-
-    @JsonProperty("title")
     public String title;
-
-    @JsonProperty("image_url")
     public String image_url;
-
-    @JsonProperty("from_date")
     public String from_date;
-
-    @JsonProperty("to_date")
     public String to_date;
-
-    @JsonProperty("price")
     public String price;
-
-    @JsonProperty("enabled")
     public Boolean enabled;
 
 }
--- VertxAutoSwagger/src/main/java/io/vertx/VertxAutoSwagger/Model/Products.java
+++ VertxAutoSwagger/src/main/java/io/vertx/VertxAutoSwagger/Model/Products.java
@@ -3,7 +3,7 @@ package io.vertx.VertxAutoSwagger.Model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.vertx.VertxAutoSwagger.Model.Product;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Products {
-    @JsonProperty("products")
     public Product[] products;
 }
